da7be4a8576358c03fe2e0694b5ef80723357800
ds-github <ds-github@users.noreply.github.com>
2014-11-01 16:46:28 +0100

    Added members to DecisionTreeNode class to distinguish between branch and leaf.

--- Exercises/01 Decision trees/Source/src/DecisionTrees/DecisionTreeNode.java
+++ Exercises/01 Decision trees/Source/src/DecisionTrees/DecisionTreeNode.java
@@ -6,4 +6,14 @@ public class DecisionTreeNode {
 	// Base decision on attribute and particular value of it
 	public AttributeDescriptor attribute;
 	public Object value;
+
+	// If the node does not branch into children, then it is a leaf node that
+	// classifies to a target value.
+	public Boolean isLeafNode;
+	public Boolean classify;
+
+	// Constructor; isLeafNode defaults to false.
+	public DecisionTreeNode() {
+		isLeafNode = false;
+	}
 }
