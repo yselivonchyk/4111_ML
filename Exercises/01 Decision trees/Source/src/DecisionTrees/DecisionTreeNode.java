266717a1b163675b7f44dcc5666de2a2b25ff4a3
ds-github <ds-github@users.noreply.github.com>
2014-11-01 19:51:17 +0100

    Added Node ID data member to DecisionTreeNode class

--- Exercises/01 Decision trees/Source/src/DecisionTrees/DecisionTreeNode.java
+++ Exercises/01 Decision trees/Source/src/DecisionTrees/DecisionTreeNode.java
@@ -1,11 +1,18 @@
 package DecisionTrees;
 
 public class DecisionTreeNode {
+
+	// Static counter variable is shared by all class instances.
+	static int idCounter = 0;
+	// Each node has an unique ID.
+	public int nodeId;
+	public Boolean parentTestResult;
+
 	public DecisionTreeNode leftChild;
 	public DecisionTreeNode rightChild;
 	// Base decision on attribute and particular value of it
 	public AttributeDescriptor attribute;
-	public Object value;
+	public Object value;	// This is already implemented in AttributeDescriptor!
 
 	// If the node does not branch into children, then it is a leaf node that
 	// classifies to a target value.
@@ -14,6 +21,8 @@ public class DecisionTreeNode {
 
 	// Constructor; isLeafNode defaults to false.
 	public DecisionTreeNode() {
+		nodeId = idCounter;
+		idCounter++;
 		isLeafNode = false;
 	}
 }
