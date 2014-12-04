8c10a7a3a1de1d478935f651585f8aac51326e15
ds-github <ds-github@users.noreply.github.com>
2014-12-04 19:02:20 +0100

    Ex 5.1: Recycled code from exercise 1.

--- Exercises/05 k-NN and Hypothesis Evaluation/src/kNNeighbors/Program.java
+++ Exercises/05 k-NN and Hypothesis Evaluation/src/kNNeighbors/Program.java
@@ -8,10 +8,60 @@ import java.util.ArrayList;
 
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 
+		String inputFileName = "./data_exercise_1.csv";
+
+		ArrayList<AttributeDescriptor> attributes = new ArrayList<>();
+		ArrayList<Example> trainingExamples = new ArrayList<>();
+		ArrayList<Example> testExamples = new ArrayList<>();
 		
+		File input = new File(inputFileName);
+		BufferedReader reader = new BufferedReader(new FileReader(input));
+		
+		AttributeDescriptor[] descriptors = buildAttributeDescriptors(reader.readLine());
+		
+		for(AttributeDescriptor attDesc : descriptors) attributes.add(attDesc);
+
+		// TODO: Start exercise 5 here.
+		
+	}
+
+
+	private static AttributeDescriptor[] buildAttributeDescriptors(
+			String descriptionLine) throws Exception {
+		// Split original string into parts dedicated to each attribute
+		String[] attributes = descriptionLine.split(",");
+		
+		AttributeDescriptor[] result = new AttributeDescriptor[attributes.length];
+		for(int i = 0; i < attributes.length; i++)
+		{
+			AttributeDescriptor descriptor = new AttributeDescriptor();
+			
+			// First part of attribute description contains Attribute name: "a:n" -> "a"
+			descriptor.name = attributes[i].split(":")[0];
+			// second part contains attribute type
+			descriptor.type = convertLetterToAttributeType(attributes[i].split(":")[1]);
+			descriptor.position = i;
+			result[i] = descriptor;
+		}
+		return result;
+	}
+
+
+	private static AttributeType convertLetterToAttributeType(String letter) throws Exception {
+		switch(letter){
+		case "n":
+			return AttributeType.Numeric;
+		case "c":
+			return AttributeType.Categorical;
+		case "b":
+			return AttributeType.Boolean;
+		case "t":
+			return AttributeType.Target;
+		default:
+				throw new Exception("Unknown attribute type: " + letter);
+		}
 	}
 
 }
